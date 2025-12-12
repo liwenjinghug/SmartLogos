a272094d757fe9885596b3f5122335d4a55033a8
rxdcjsyzds <2439815974@qq.com>
2025-12-12 23:17:25 +0800

    Delete src directory

--- src/main/java/com/smartlogos/note/repository/NoteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.smartlogos.note.repository;
-
-import com.smartlogos.note.entity.Note;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface NoteRepository extends JpaRepository<Note, Long> {
-    Optional<Note> findByDocumentId(Long documentId);
-}
